9d137ae35a4452cff5eaa8e12565ae61ca83def8
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-04-21 00:51:50 -0300

    .env working

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,15 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const Dotenv = require('dotenv-webpack');
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+module.exports = {
+  environment.toWebpackConfig()
+  // plugins: [
+  //   new Dotenv(
+  //     'process.env': {
+  //       'REACT_APP_FOOTBALL_KEY': JSON.stringify(process.env.REACT_APP_FOOTBALL_KEY)
+  //     }
+  //   )
+  // ]
+}
