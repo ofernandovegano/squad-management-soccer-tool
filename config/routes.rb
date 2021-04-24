9313bd795cbe3952acc193f3694bf19323aaf382
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-04-24 00:44:21 -0300

    change size of letters top5

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get '/', to: 'pages#home'
   get '/new', to: 'pages#home'
   get '/reload_teams', to: 'pages#reload_teams'
-  get '/reload_avg_age_players', to: 'pages#reload_avg_age_players'
+  get '/reload_avg_age_players_part_one', to: 'pages#reload_avg_age_players_part_one'
+  get '/reload_avg_age_players_part_two', to: 'pages#reload_avg_age_players_part_two'
+  get '/reload_avg_age_players_part_three', to: 'pages#reload_avg_age_players_part_three'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
