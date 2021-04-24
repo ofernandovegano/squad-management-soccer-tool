e7d59df27a8ba31ec324ee7df9d0afaa23e60ecd
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-04-24 20:38:19 -0300

    add state to formation players

--- app/javascript/squad-app/containers/formation_players_form.jsx
+++ app/javascript/squad-app/containers/formation_players_form.jsx
@@ -8,25 +8,47 @@ import Player from '../components/player'
 
 class FormationPlayersForm extends Component {
 
-  Player() {
-    return <Player />
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectValue: '4-2-3-1'
+    };
+  }
+
+  Player(numberPlayers) {
+    for (let i = 0; i < numberPlayers; i++) {
+      return <Player />
+    }
+  }
+
+  handleChange(e){
+    const formation = e.target.value;
+    // this.setState({ attack: parseInt(formation.slice(-1)) });
+    // if (formation.length === 5) {
+    //   this.setState({ middleAttack: 0 })
+    // } else{
+    //   this.setState({ middleAttack: parseInt(formation.slice(4, 5)) });
+    // }
+    // this.setState({ middle: parseInt(formation.slice(2, 3)) });
+    // this.setState({ defense: parseInt(formation.slice(0, 1)) });
+    console.log(formation)
   }
 
   render() {
     return (
       <div className="col-12 col-md-6">
         <label for="formation" className='formation-header'>Formation</label>
-        <select id="formation" name="formation">
+        <select id="formation" name="formation" onChange={this.handleChange} defaultValue={this.state.selectValue}>
+          <option value="4-2-3-1">4-2-3-1</option>
           <option value="3-2-2-3">3-2-2-3</option>
           <option value="3-2-2-1">3-2-2-1</option>
-          <option value="3-4-3">3-4-3</option>
-          <option value="3-5-2">3-5-2</option>
-          <option value="4-2-3-1">4-2-3-1</option>
-          <option value="4-3-1-1">4-3-1-1</option>
-          <option value="4-3-2">4-3-2</option>
-          <option value="4-4-2">4-4-2</option>
-          <option value="4-5-1">4-5-1</option>
-          <option value="5-4-1">5-4-1</option>
+          <option value="4-3-1-1">4-3-2-1</option>
+          {/* <option value="3-4-3">3-4-3</option> */}
+          {/* <option value="3-5-2">3-5-2</option> */}
+          {/* <option value="4-3-2">4-3-2</option> */}
+          {/* <option value="4-4-2">4-4-2</option> */}
+          {/* <option value="4-5-1">4-5-1</option> */}
+          {/* <option value="5-4-1">5-4-1</option> */}
         </select>
         <div className='formation-field'>
 
@@ -48,6 +70,7 @@ class FormationPlayersForm extends Component {
 
 function mapStateToProps(state) {
   return {
+
   };
 }
 
